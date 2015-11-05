742e48608483dc5d73f8151a517d0c5c85c44665
Eddie​ <ejung716@gmail.com>
2015-11-05 09:55:17 -0800

    timestamp for comments working

--- walking_dead/app/controllers/posts_controller.rb
+++ walking_dead/app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @posts = Post.paginate(:page => params[:page], :per_page => 10)
+    @posts = Post.paginate(:page => params[:page], :per_page => 15)
   end
 
   def show
